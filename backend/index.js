2edea1a6b624d14e91b44b777eabb018bd387c41
pawor <pawor.emi@gmail.com>
2021-10-20 02:10:36 +0700

    create backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-confirm
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL , {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch(err => console.log(err));
+
+app.use("/api/auth", authRoute);
+app.listen(8080, () => {
+    console.log("Backend server is running!");
+});
+
